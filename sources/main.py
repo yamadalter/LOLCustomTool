294d30cdf1c4e7e21ed610f7ed306e51c0e42134
yamadalter <yamadalternative515@gmail.com>
2024-10-27 03:49:21 +0900

    fix main.py

---
+++ sources/main.py
@@ -0,0 +1,18 @@
+import sys
+import asyncio
+
+from PyQt6.QtWidgets import QApplication
+from gui import MainWindow  # gui.py から MainWindow をインポート
+import qasync
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    loop = qasync.QEventLoop(app)
+    asyncio.set_event_loop(loop)
+
+    try:
+        sys.exit(loop.run_forever())
+    finally:
+        loop.close()
